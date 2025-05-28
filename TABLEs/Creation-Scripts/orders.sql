f51eded17ce0d3e950f8065dd29f15f92b2ed232
lsaperi <lsaperi>
2025-05-28 08:55:11 -0300

    dsdas; TaskID: task2

--- TABLEs/Creation-Scripts/orders.sql
+++ TABLEs/Creation-Scripts/orders.sql
@@ -3,6 +3,5 @@ CREATE TABLE `orders` (
   `client_id` int DEFAULT NULL,
   `product` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_orders_clients` (`client_id`),
-  CONSTRAINT `fk_orders_clients` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+  KEY `fk_orders_clients` (`client_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
