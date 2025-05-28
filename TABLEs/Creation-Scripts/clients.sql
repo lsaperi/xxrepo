7abd75e2ce803df748acfbb80a48b85ed356e1a2
lsaperi <lsaperi>
2025-05-28 09:16:09 -0300

    test lucas; TaskID: task3

--- TABLEs/Creation-Scripts/clients.sql
+++ TABLEs/Creation-Scripts/clients.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `clients` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
+  `lucascol` varchar(45) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_cli_ord` (`name`),
+  CONSTRAINT `fk_cli_ord` FOREIGN KEY (`name`) REFERENCES `orders` (`product`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
--- TABLEs/Creation-Scripts/orders.sql
+++ TABLEs/Creation-Scripts/orders.sql
@@ -3,5 +3,7 @@ CREATE TABLE `orders` (
   `client_id` int DEFAULT NULL,
   `product` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_orders_clients` (`client_id`)
+  KEY `fk_orders_clients` (`client_id`),
+  KEY `idx_orders_product` (`product`),
+  CONSTRAINT `fk_orders_clients` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
