db68659a9e8006f424099790ba9390a26383427d
lsaperi <lsaperi>
2025-06-13 10:13:03 -0300

    fds; TaskID: Retest1

---
+++ TABLEs/Creation-Scripts/buildings.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `buildings` (
+  `idbuildings` int NOT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idbuildings`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
